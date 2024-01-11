53cfded5c5ce8ecf9d0e2e9a990937c632f05eeb
yingrong <yingrong.hu@gmail.com>
2024-01-11 09:09:59 +0800

    chore: init project

---
+++ src/main/java/com/example/bpp/BppApplication.java
@@ -0,0 +1,13 @@
+package com.example.bpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bpp/BppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
